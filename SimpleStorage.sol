981120cdb617be193c887f211815f0e688cf5ae1
Atandanafiu <atandanafiu21@gmail.com>
2022-02-10 21:57:27 +0100

    First deployment

---
+++ SimpleStorage.sol
@@ -0,0 +1,28 @@
+//"SPDX-License-sIdentifier: MIT"
+
+pragma solidity 0.8.9;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    struct People {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+    }
+}
