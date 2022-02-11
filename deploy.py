68ef245803be8e7ae6403ee86ac3041e3230c90d
Atandanafiu <atandanafiu21@gmail.com>
2022-02-11 02:59:48 +0100

    adding abi and bytecode to compile, also add json file

--- deploy.py
+++ deploy.py
@@ -1,3 +1,40 @@
+from dis import Bytecode
+from solcx import compile_standard, install_solc
+import json
+
+install_solc("0.8.9")
+
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+
+
+# compile our solidity
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.soucreMap"],
+                }
+            }
+        },
+    },
+    solc_version="0.8.9",
+)
+
+# To compile
+with open("compiled_sol.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+print(bytecode)
